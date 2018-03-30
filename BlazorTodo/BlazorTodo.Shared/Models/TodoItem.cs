d9978647eb3cc4498019ccf2e52dd6ac424db06d
Quentin JACQUET <qjacquet@smag.local>
2018-03-30 16:26:06 +0200

    first commit

---
+++ BlazorTodo/BlazorTodo.Shared/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorTodo.Shared.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsInEdit { get; set; }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+# Todo list sample with Blazor
